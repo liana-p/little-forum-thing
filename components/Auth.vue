07c6f56d9f2f27e63c801cc363626c1ec410391f
Liana Pigeot <32243840+liana-p@users.noreply.github.com>
2022-11-15 21:40:03 +0000

    new user auth fixed

--- components/Auth.vue
+++ components/Auth.vue
@@ -4,7 +4,12 @@
       <h1 class="header">Supabase + Nuxt 3</h1>
       <p class="description">Sign in via magic link with your email below</p>
       <div>
-        <input class="inputField" type="email" placeholder="Your email" v-model="email" />
+        <input
+          class="inputField"
+          type="email"
+          placeholder="Your email"
+          v-model="email"
+        />
       </div>
       <div>
         <input
@@ -19,20 +24,30 @@
 </template>
 
 <script setup>
-  const supabase = useSupabaseClient()
+import { v4 as uuidv4 } from "uuid";
 
-  const loading = ref(false)
-  const email = ref('')
-  const handleLogin = async () => {
-    try {
-      loading.value = true
-      const { error } = await supabase.auth.signInWithOtp({ email: email.value })
-      if (error) throw error
-      alert('Check your email for the login link!')
-    } catch (error) {
-      alert(error.error_description || error.message)
-    } finally {
-      loading.value = false
-    }
+const supabase = useSupabaseClient();
+
+const loading = ref(false);
+const email = ref("");
+const handleLogin = async () => {
+  try {
+    loading.value = true;
+    const { error } = await supabase.auth.signInWithOtp({
+      email: email.value,
+      options: {
+        data: {
+          username: uuidv4(),
+          display_name: "New User",
+        },
+      },
+    });
+    if (error) throw error;
+    alert("Check your email for the login link!");
+  } catch (error) {
+    alert(error.error_description || error.message);
+  } finally {
+    loading.value = false;
   }
+};
 </script>
